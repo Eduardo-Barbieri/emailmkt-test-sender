33803f08805fede7fa030619f1ef3951b2dfe8f2
Eduardo Côrte Real Barbieri <edu.cortereal@hotmail.com>
2021-10-06 21:37:53 -0300

    added url as a variable

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,11 +1,10 @@
 const fetch = require('node-fetch')
 
-async function main(){
-	const res = await fetch(
-	'http://s3.amazonaws.com/mailmkt.aviva/0539_verao_2122_email_3_b2c_RQ/0539_verao_2122_email_3_b2c_RQ.html');
+async function main(url){
+	const res = await fetch(url);
 	const body = await res.text();
 	console.log(body); 
 	return body;
 }
 
-module.exports = main
+module.exports = main;
--- src/routes/router.js
+++ src/routes/router.js
@@ -6,7 +6,7 @@ router = express.Router()
 router.get("/", (req,res)=>{
 	body = main();
 	res.status(200).json({
-		menssagem:"servidor online"
+		menssagem:"url fetched"
 	});
 });
 
